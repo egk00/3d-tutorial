7dc75a024f56f399a0a67bd61d51af4e28470de7
egk00 <31694207+egk00@users.noreply.github.com>
2019-09-01 18:58:25 +0900

    .

--- 3d tutorial/Assets/NewBehaviourScript.cs
+++ 3d tutorial/Assets/NewBehaviourScript.cs
@@ -1,22 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-        }
+        
+    }
+
+    public void Call()
+    {
+        Debug.Log("!!!");
     }
 }
---
+++ 3d tutorial/Assets/TestDrag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrgHandle, IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
